3fac75b9a72d41662be7f3b1c46664be9ffd2838
paulpolash <premacaitanyapranadas@gmail.com>
2026-03-23 13:59:38 +0600

    creating room for db

---
+++ app/src/main/java/com/example/androidarchitecture/data/local/UserDao.kt
@@ -0,0 +1,17 @@
+package com.example.androidarchitecture.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.androidarchitecture.data.model.Users
+
+@Dao
+interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUsers(users: List<Users>)
+
+    @Query("SELECT * FROM Users")
+    fun getAllUsers():LiveData<List<Users>>
+}
--- app/src/main/java/com/example/androidarchitecture/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/androidarchitecture/data/repository/UserRepository.kt
@@ -1,5 +1,7 @@
 package com.example.androidarchitecture.data.repository
 
+import androidx.lifecycle.LiveData
+import com.example.androidarchitecture.data.local.UserDao
 import com.example.androidarchitecture.data.model.CreatePostRequest
 import com.example.androidarchitecture.data.model.Response
 import com.example.androidarchitecture.data.model.User
@@ -8,11 +10,18 @@ import com.example.androidarchitecture.data.remote.ApiService
 import com.example.androidarchitecture.data.remote.RetrofitInstance
 import retrofit2.http.POST
 
-class UserRepository(private val apiService: ApiService = RetrofitInstance.api) {
+class UserRepository(private val apiService: ApiService = RetrofitInstance.api, private val dao: UserDao) {
     suspend fun getUsers(): List<User>{
         return apiService.getUsers()
     }
     suspend fun createPost(createPostRequest: CreatePostRequest): Response {
         return apiService.createPost(createPostRequest)
+    }
+
+    val users: LiveData<List<Users>> = dao.getAllUsers()
+
+    suspend fun fetchAndSaveUsers(){
+        val apiUsers = RetrofitInstance.api.getUsers()
+//        dao.insertUsers(apiUsers)
     }
 }
